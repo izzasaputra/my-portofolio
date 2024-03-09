e28b103c1d29d00fed858e64442c98a095167809
izzasaputra <izzasaputra17@gmail.com>
2024-03-10 00:02:57 +0700

    change about section

--- src/container/homepage/about/index.tsx
+++ src/container/homepage/about/index.tsx
@@ -4,8 +4,13 @@ import styles from "./about.module.scss";
 import { Row, Col } from "antd";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Image from "next/image";
+import Tilt from "react-parallax-tilt";
+import useResponsive from "@/hooks/responsive";
 
 export default function About() {
+  const useScreen = useResponsive();
+
   useEffect(() => {
     AOS.init();
     AOS.refresh();
@@ -13,18 +18,80 @@ export default function About() {
 
   return (
     <div className={styles.about_wrapper}>
-      <Row data-aos="fade-up-left">
-        <Col xs={24} sm={12} lg={12}></Col>
-        <Col xs={24} sm={12} lg={12}>
-          <div className={styles.title}>Who am I?</div>
-          <div className={styles.description}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Perspiciatis placeat tenetur expedita maxime ipsam, aut atque amet
-            voluptatibus recusandae, sint molestias. Hic obcaecati excepturi
-            assumenda dolor porro deleniti error quas.
+      {useScreen.mobile ? (
+        <Row>
+          <Col style={{ width: "100%" }}>
+            <div
+              className={styles.title}
+              data-aos="zoom-in-up"
+              data-aos-duration="300"
+            >
+              Who am I?
+            </div>
+          </Col>
+          <Col
+            data-aos="zoom-in-up"
+            data-aos-delay="500"
+            style={{ width: "100%", display: "flex", justifyContent: "center" }}
+          >
+            <Tilt>
+              <Image src="/image/me.png" width="220" height="230" alt="" />
+            </Tilt>
+          </Col>
+          <Col>
+            <div
+              className={styles.description}
+              data-aos="fade-up-left"
+              data-aos-delay="600"
+            >
+              I'm Izza Saputra, an experienced developer known for my expertise
+              and innovation. With a proven track record, I specialize in
+              crafting sleek user interfaces and robust backend solutions.
+            </div>
+          </Col>
+        </Row>
+      ) : (
+        <>
+          <div className={styles.title} data-aos="zoom-in">
+            Who am I?
           </div>
-        </Col>
-      </Row>
+          <div
+            style={{
+              width: 300,
+              position: "absolute",
+              top: "160px",
+              left: "250px",
+              fontSize: "16px",
+            }}
+            data-aos="fade-up-left"
+            data-aos-delay="900"
+          >
+            I'm Izza Saputra, an experienced developer known for my expertise
+            and innovation. With a proven track record, I specialize in crafting
+            sleek user interfaces and robust backend solutions.
+          </div>
+          <div className={styles.photo} data-aos="zoom-in" data-aos-delay="500">
+            <Tilt>
+              <Image src="/image/me.png" width="330" height="340" alt="" />
+            </Tilt>
+          </div>
+          <div
+            style={{
+              width: 300,
+              position: "absolute",
+              top: "400px",
+              right: "150px",
+              fontSize: "16px",
+            }}
+            data-aos="fade-up-right"
+            data-aos-delay="900"
+          >
+            Dedicated to delivering tailored excellence, I'm committed to
+            exceeding expectations and achieving outstanding results for every
+            project.
+          </div>
+        </>
+      )}
     </div>
   );
 }
